11d77ef48d8f71d9c3af991a6ee11050bf2f8bd1
keerthana k <48295289+keerthu2092@users.noreply.github.com>
2022-04-22 23:06:39 +0530

    Update PrimeNumber.java

--- Week1.day1/PrimeNumber.java
+++ Week1.day1/PrimeNumber.java
@@ -3,11 +3,9 @@ package week1.day1;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		
 		int i,m=0,flag=0;   
 		  int num=13;
 		  m=num/2;    
-		 
 		   for(i=2;i<=m;i++){    
 		    if(num%i==0){    
 		     System.out.println(num+" is not prime number");    
