3917afb437bf9192c3f461bb893f347b0ba15d6c
keerthana k <48295289+keerthu2092@users.noreply.github.com>
2022-04-22 23:07:52 +0530

    Update MissingElementInAnArray.java

--- Week1.day2/MissingElementInAnArray.java
+++ Week1.day2/MissingElementInAnArray.java
@@ -3,10 +3,7 @@ package week1.day2;
 import java.util.Arrays;
 
 public class MissingElementInAnArray {
-
-
 	public static void main(String[] args) {
-		;
         int[] array1 = {1,2,3,4,7,6,8};
 		Arrays.sort(array1);
         for(int i=0;i<array1.length-1;i++)
